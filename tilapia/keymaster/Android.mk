333b9da7b466fa8babd201156fa88f132b113e25
Ryan Wilson <rjwil1086@gmail.com>
2015-01-04 00:45:19 -0600

    Revert "Fix for CM12"
    This reverts commit c8f11dbb5200e1848e48db85e7733dd5dbfeac81.

--- tilapia/keymaster/Android.mk
+++ tilapia/keymaster/Android.mk
@@ -29,7 +29,6 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES := \
 	libcore/include \
 	external/openssl/include \
-	libnativehelper/include \
 	$(LOCAL_PATH)/../security/tf_sdk/include
 
 LOCAL_CFLAGS := -fvisibility=hidden -Wall -Werror
